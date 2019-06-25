d49d49709cf4bf6afb5d7b1427a42c12e19d91cf
Grzegorz Kułaga <poczta@grzegorzkulaga.pl>
2019-06-25 14:21:34 +0200

    Lista

--- Lista/Lista/Program.cs
+++ Lista/Lista/Program.cs
@@ -10,6 +10,61 @@ namespace Lista
     {
         static void Main(string[] args)
         {
+            List<string> gatunki = new List<string>();
+
+            gatunki.Add("kot");
+            gatunki.Add("pies");
+            gatunki.Add("słoń");
+            gatunki.Add("nosorożeć");
+            gatunki.Add("zebra");
+
+            foreach(string gatunek in gatunki)
+            {
+                Console.WriteLine(gatunek);
+            }
+
+            //gatunki.RemoveAt(0);
+            gatunki.Remove("zebra");
+            gatunki.Remove("kot");
+            foreach (string gatunek in gatunki)
+            {
+                Console.WriteLine(gatunek);
+            }
+
+            gatunki.Insert(3, "mucha");
+            gatunki.Insert(1, "gazela");
+            foreach (string gatunek in gatunki)
+            {
+                Console.WriteLine(gatunek);
+            }
+
+            gatunki.Remove("gazela");
+            foreach (string gatunek in gatunki)
+            {
+                Console.WriteLine(gatunek);
+            }
+            Console.WriteLine(gatunki.IndexOf("mucha"));
+
+            gatunki.Reverse();
+            foreach (string gatunek in gatunki)
+            {
+                Console.WriteLine(gatunek);
+            }
+
+            gatunki.Sort();
+            foreach (string gatunek in gatunki)
+            {
+                Console.WriteLine(gatunek);
+            }
+
+            if (gatunki.Contains("krowa", StringComparer.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
         }
     }
 }
