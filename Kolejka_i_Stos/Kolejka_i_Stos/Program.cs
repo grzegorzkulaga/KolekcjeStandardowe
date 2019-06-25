4a7df350d80542ee698d83f3c2b6a49f6630d9b0
Grzegorz Kułaga <poczta@grzegorzkulaga.pl>
2019-06-25 14:43:59 +0200

    Zadanie 2

--- Kolejka_i_Stos/Kolejka_i_Stos/Program.cs
+++ Kolejka_i_Stos/Kolejka_i_Stos/Program.cs
@@ -10,6 +10,61 @@ namespace Kolejka_i_Stos
     {
         static void Main(string[] args)
         {
+            Queue<string> litery = new Queue<string>();
+            Stack<string> cyfry = new Stack<string>();
+
+            cyfry.Push("1");
+            cyfry.Push("2");
+            cyfry.Push("3");
+            cyfry.Push("4");
+
+            litery.Enqueue("a");
+            litery.Enqueue("b");
+            litery.Enqueue("c");
+            litery.Enqueue("d");
+
+            foreach(string cyfra in cyfry)
+            {
+                Console.WriteLine(cyfra);
+            }
+
+            foreach (string litera in litery)
+            {
+                Console.WriteLine(litera);
+            }
+
+            litery.Enqueue(cyfry.Pop());
+            litery.Enqueue(cyfry.Pop());
+
+            foreach (string litera in litery)
+            {
+                Console.WriteLine(litera);
+            }
+
+            Console.WriteLine(litery.Peek());
+            Console.WriteLine(cyfry.Peek());
+
+            cyfry.Pop();
+            cyfry.Pop();
+
+            litery.Enqueue("e");
+
+            foreach (string cyfra in cyfry)
+            {
+                Console.WriteLine(cyfra);
+            }
+
+            foreach (string litera in litery)
+            {
+                Console.WriteLine(litera);
+            }
+
+            litery.Enqueue(cyfry.Pop());
+            foreach (string litera in litery)
+            {
+                Console.WriteLine(litera);
+            }
+
         }
     }
 }
